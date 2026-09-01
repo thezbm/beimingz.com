d1c220bf8c1bef265ca3ad61e85e6fa8bc666073
Beiming Zhang <thebeimingzhang@outlook.com>
2026-09-01 00:49:30 -0500

    Add LaTeX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import expressiveCode from "astro-expressive-code";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://www.beimingz.com",
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   integrations: [expressiveCode(), mdx(), sitemap()],
 });

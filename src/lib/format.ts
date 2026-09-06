20a032a20f7b67e013cc1afb37ab5831c91a0e19
Beiming Zhang <thebeimingzhang@outlook.com>
2026-09-06 16:22:25 -0500

    Fix date display format

--- src/lib/format.ts
+++ src/lib/format.ts
@@ -3,6 +3,7 @@ export function formatDate(
   style: "short" | "long" = "short",
 ): string {
   return date.toLocaleDateString("en-US", {
+    timeZone: "UTC",
     year: "numeric",
     month: style === "long" ? "long" : "short",
     day: "numeric",
